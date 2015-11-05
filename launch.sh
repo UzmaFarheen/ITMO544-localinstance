43bdb4e0a9b449bd34e623c28e4db554a9a82648
UzmaFarheen <unln@hawk.iit.edu>
2015-11-05 00:41:08 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4  --subnet-id $5 --key-name $6 --associate-public-ip-address  --debug 
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4  --subnet-id $5 --key-name $6 --iam-profile $7 --associate-public-ip-address  --debug 
