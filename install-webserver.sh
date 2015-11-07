7ade7d0a631e5a3c30a5bcfeb6d65e0a7e3a3f90
UzmaFarheen <unln@hawk.iit.edu>
2015-11-07 17:15:56 -0600

    Create install-webserver.sh

---
+++ install-webserver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
+
+curl -sS https://getcomposer.org/installer | php
+
+php composer.phar require aws/aws-sdk-php
+
+cp ./ITMO-547/html.html /var/www/html
+ 
+echo"Hello, My Name is UZMA FARHEEN" > /tmp/hello.txt
