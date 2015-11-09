38860c3035090a98a2bbe93499d0bb7a4a66a90c
UzmaFarheen <unln@hawk.iit.edu>
2015-11-09 09:03:02 -0600

    Update install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,9 +3,13 @@
 sudo apt-get -y update
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
 
-curl -sS https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
-php composer.phar require aws/aws-sdk-php
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 
 mv ./ITMO-547/html.html /var/www/html
  
