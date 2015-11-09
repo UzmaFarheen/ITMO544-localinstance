d99a967d9121b79d191f74a8b8b4ca4620b3ca47
UzmaFarheen <unln@hawk.iit.edu>
2015-11-09 10:13:50 -0600

    Update install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -7,10 +7,6 @@ curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
-sudo mv vendor /var/www/html &> /tmp/movevendor.txt
-
-sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
-
 mv ./ITMO-547/html.html /var/www/html
  
 echo"Hello, My Name is UZMA FARHEEN" > /tmp/hello.txt
