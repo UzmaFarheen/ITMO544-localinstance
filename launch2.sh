2d32f5bbcb8b2c9a41629166d8f6dff8a0400752
UzmaFarheen <unln@hawk.iit.edu>
2015-11-09 10:22:04 -0600

    Launch Configuration 2

--- launch2.sh
+++ launch2.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-
-
 aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-id $4 --subnet-id $5 --associate-public-ip-address --iam-instance-profile $7 --user-data install-webserver.sh --debug
