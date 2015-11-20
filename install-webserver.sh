74be9f0b29ac303b91ff201ad755d150d33ed9f0
UzmaFarheen <unln@hawk.iit.edu>
2015-11-20 15:46:06 -0600

    updated

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/UzmaFarheen/ITMO544-localinstance.git
 git clone https://github.com/UzmaFarheen/MP1-PHP.git
@@ -13,4 +13,4 @@ sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 mv ./ITMO-547/html.html /var/www/html
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
  
-echo"Hello, My Name is UZMA FARHEEN" > /tmp/hello.txt
+echo "Hello, My Name is UZMA FARHEEN" > /tmp/hello.txt
