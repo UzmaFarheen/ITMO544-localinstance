dd1129612c6f2d199d0edd2f24b7bc47679050ad
UzmaFarheen <unln@hawk.iit.edu>
2015-11-21 15:25:01 -0600

    updated

--- install-webserver.sh
+++ install-webserver.sh
@@ -6,11 +6,17 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 git clone https://github.com/UzmaFarheen/ITMO544-localinstance.git
 git clone https://github.com/UzmaFarheen/MP1-PHP.git
 
+mv ./ITMO-547/html.html /var/www/html
+sudo mv ./MP1-PHP/*.css /var/www/html
+sudo mv ./MP1-PHP/*.js /var/www/html
+sudo mv ./MP1-PHP/*.php /var/www/html
+
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
-mv ./ITMO-547/html.html /var/www/html
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
- 
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
 echo "Hello, My Name is UZMA FARHEEN" > /tmp/hello.txt
