c476d1365469ccd6bd1ede07ac2f7f7a3bbb1874
UzmaFarheen <unln@hawk.iit.edu>
2015-11-21 16:38:22 -0600

    added

--- install-webserver.sh
+++ install-webserver.sh
@@ -6,7 +6,8 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 git clone https://github.com/UzmaFarheen/ITMO544-localinstance.git
 git clone https://github.com/UzmaFarheen/MP1-PHP.git
 
-mv ./ITMO-547/html.html /var/www/html
+sudo mv ./MP1-PHP/index.html /var/www/html
+sudo mv ./MP1-PHP/page2.html /var/www/html
 sudo mv ./MP1-PHP/*.css /var/www/html
 sudo mv ./MP1-PHP/*.js /var/www/html
 sudo mv ./MP1-PHP/*.php /var/www/html
