79578a633c5055196b0d19dff1f28d5938c15285
UzmaFarheen <unln@hawk.iit.edu>
2015-12-14 04:17:51 -0600

    reverted back to original

--- loadbalancer.sh
+++ loadbalancer.sh
@@ -1,16 +1,18 @@
-!/bin/bash
-declare -a ARR
+#!/bin/bash
+declare -a array
 
-mapfile -t ARR < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-id $4 --subnet-id $5 --key-name $6 --iam-instance-profile $7 --associate-public-ip-address --user-data install-webserver.sh) 
+mapfile -t array< <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-id $4 --subnet-id $5 --key-name $6 --associate-public-ip-address --user-data install-webserver.sh --iam-instance-profile Name=$7  --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g") 
 
 #ec2 wait command-
-aws ec2 wait instance-running --instance-ids ${ARR[@]}
+aws ec2 wait instance-running --instance-ids ${array[@]}
 
 #load balancer creation
 aws elb create-load-balancer --load-balancer-name ITMO-544-MP-loadbalancer --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --security-groups $4 --subnet $5 
-
+echo -e "\n Finished launching ELB and sleeping 25 seconds"
+for i in {0..25}; do echo -ne '.'; sleep 1;done
+echo "\n"
 #load balancer registration
-aws elb register-instances-with-load-balancer --load-balancer-name ITMO-544-MP-loadbalancer --instances ${ARR[@]} 
+aws elb register-instances-with-load-balancer --load-balancer-name ITMO-544-MP-loadbalancer --instances ${array[@]} 
 
 #health check policy configuration
 aws elb configure-health-check --load-balancer-name ITMO-544-MP-loadbalancer --health-check Target=HTTP:80/png,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
@@ -19,10 +21,10 @@ aws elb configure-health-check --load-balancer-name ITMO-544-MP-loadbalancer --h
 aws elb create-lb-cookie-stickiness-policy --load-balancer-name ITMO-544-MP-loadbalancer --policy-name ITMO-544-cookiepolicy --cookie-expiration-period 60
 
 #launch configuration creattion
-aws autoscaling create-launch-configuration --launch-configuration-name itmo544-launch-config --image-id $1 --count $2 --instance-type $3 --security-groups $4  --key-name $6  --user-data install-webserver.sh --iam-instance-profile $7
+aws autoscaling create-launch-configuration --launch-configuration-name itmo544-launch-config --image-id $1 --instance-type $3 --security-groups $4  --key-name $6  --user-data install-webserver.sh --iam-instance-profile "$7"
 
 #Autoscaling group creation
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-autoscaling --launch-configuration-name itmo544-launch-config --load-balancer-names ITMO-544-MP-loadbalancer  --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $5 
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-autoscaling --launch-configuration-name itmo544-launch-config --load-balancer-names ITMO-544-MP-loadbalancer  --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $5
 
 #AutoScaling Policy-Increase
 
@@ -34,12 +36,12 @@ DECREASE=(`aws autoscaling put-scaling-policy --auto-scaling-group-name itmo-544
 
 #Cloud Watch Metric 
 
-aws cloudwatch put-metric-alarm --alarm-name Add --alarm-description "CPU exceeds 30 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold --evaluation-periods 1 --unit Percent --dimensions "Name=itmo-544-autoscaling" --alarm-actions $INCREASE
+aws cloudwatch put-metric-alarm --alarm-name Add --alarm-description "CPU exceeds 30 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold --evaluation-periods 1 --unit Percent --dimensions "Name=AutoScalingGroupName,Value=itmo-544-autoscaling" --alarm-actions $INCREASE
 
-aws cloudwatch put-metric-alarm --alarm-name Reduce --alarm-description "CPU falls below 10 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 10 --comparison-operator LessThanOrEqualToThreshold --evaluation-periods 1 --unit Percent --dimensions "Name=itmo-544-autoscaling" --alarm-actions $DECREASE
+aws cloudwatch put-metric-alarm --alarm-name Reduce --alarm-description "CPU falls below 10 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 10 --comparison-operator LessThanOrEqualToThreshold --evaluation-periods 1 --unit Percent --dimensions "Name=AutoScalingGroupName,Value=itmo-544-autoscaling" --alarm-actions $DECREASE
+
 
 #database subnet creation-
-
 aws rds create-db-subnet-group --db-subnet-group-name ITMO544DBSubnet --subnet-ids subnet-b2b1e999 subnet-42351f1b --db-subnet-group-description 544subnet
 
 #AWS RDS instances creation
@@ -47,4 +49,3 @@ aws rds create-db-instance --db-instance-identifier mp1 --engine MySQL --db-name
 
 #read replica creation
 aws rds create-db-instance-read-replica --db-instance-identifier mp1-replica --source-db-instance-identifier mp1
-
